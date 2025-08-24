7e583ad76b23f704e09de2702bf45b7ea3faa3db
neo <javohirtech@gmail.com>
2025-08-24 20:20:36 +0500

    feat: add search icon and update button import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {Button} from "noor-ui";
+import {Search} from "lucide-react";
 
 const App = () => {
   return (
       <div className={"p-4"}>
         app
+        <Search size={120} className={"bg-red-500"}/>
         <Button>Salom</Button>
       </div>
   )
