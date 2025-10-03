7b4f39cbb9e85a003d3eecb69a84de63065b0a7a
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-10-03 22:50:50 +0500

    feat: enhance sidebar with structured sections and dynamic link highlighting

---
+++ src/app/docs/button/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default page
--- src/widgets/sidebar/index.tsx
+++ src/widgets/sidebar/index.tsx
@@ -1,42 +1,60 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Badge, List, ListItem, Typography } from "noor-ui";
-import React from "react";
 
 export const Sidebar = () => {
+  const pageName = usePathname();
+
   return (
-    <div className="col-span-3 w-full space-y-4 h-screen overflow-scroll">
-      <Typography>Get started</Typography>
-      <List className="text-sm">
-        <ListItem selected>Installition</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-      </List>
-
-      <Typography>Customization</Typography>
+    <div className="col-span-3 w-full text-sm px-4 space-y-4 h-screen overflow-scroll">
+      <Typography variant="h6">Get started</Typography>
       <List>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
+        <ListItem>Installition</ListItem>
       </List>
 
-      <Typography>Components</Typography>
+      <Typography variant="h6">Customization</Typography>
       <List>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
+        <ListItem>Theming</ListItem>
+        <ListItem>Colors</ListItem>
+        <ListItem>Fonts</ListItem>
+        <ListItem>Shadows</ListItem>
+        <ListItem>Screen</ListItem>
       </List>
 
-      <Typography>Forms</Typography>
+      <Typography variant="h6">Components</Typography>
       <List>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
-        <ListItem>salom</ListItem>
+        <ListItem>Accordion</ListItem>
+        <ListItem>Alert</ListItem>
+        <ListItem>Avatar</ListItem>
+        <ListItem>Badge</ListItem>
+        <ListItem>Breadcrumbs</ListItem>
+        <ListItem selected={pageName === "/docs/button"}>
+          <Link href={"/docs/button"}>Button</Link>
+        </ListItem>
+        <ListItem>IconButton</ListItem>
+        <ListItem>List</ListItem>
+        <ListItem>Loader</ListItem>
+        <ListItem>Menu</ListItem>
+        <ListItem>Popover</ListItem>
+        <ListItem>Progress</ListItem>
+        <ListItem>Stepper</ListItem>
+        <ListItem>Tabs</ListItem>
+        <ListItem>Typography</ListItem>
+        <ListItem>Video</ListItem>
+        <ListItem>BgPattern</ListItem>
       </List>
 
-      <Typography className="items-center justify-between flex">
+      <Typography variant="h6">Forms</Typography>
+      <List>
+        <ListItem>Input</ListItem>
+        <ListItem>Textarea</ListItem>
+        <ListItem>Checkbox</ListItem>
+        <ListItem>Radio</ListItem>
+        <ListItem>Switch</ListItem>
+      </List>
+
+      <Typography variant="h6" className="items-center justify-between flex">
         Plugins <Badge variant="text">Soon</Badge>
       </Typography>
     </div>
